8af1d7f3d27771251f91c1b0a91eb92e6212daab
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-09-03 17:52:11 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,44 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  i = 0 
+  
+  while i < array.length do 
+    if array[i] == value_to_find
+      return i 
+    end 
+    
+    i += 1
+  end
+  
+  return nil
 end
 
 def find_max_value(array)
-  # Add your solution here
+  i = 0 
+  
+  highest_num = array[0]
+  
+  while i < array.length do 
+    if array[i] > highest_num
+      highest_num = array[i]
+    end 
+    
+    i += 1
+  end 
+  
+  return highest_num
 end
 
 def find_min_value(array)
-  # Add your solution here
+  i = 0 
+  
+  min_value = array[0]
+  
+  while i < array.length do 
+    if array[i] < min_value
+      min_value = array[i]
+    end 
+    i += 1 
+  end
+  
+  return min_value
 end
